7f1970dbf91dad06535c793f5af53cd7838f309e
Svyatoslav Popov <sdpopov@keyvar.com>
2022-10-29 00:46:22 +0300

    KvLine3 has been refactored.

--- Sources/kvGeometry/KvLine3.swift
+++ Sources/kvGeometry/KvLine3.swift
@@ -33,27 +33,40 @@ public struct KvLine3<Math : KvMathScope> {
     public typealias Scalar = Math.Scalar
     public typealias Vector = Math.Vector3
     public typealias Coordinate = Vector
-    public typealias Quaternion = Math.Quaternion
 
     public typealias Transform = KvTransform3<Math>
     public typealias AffineTransform = KvAffineTransform3<Math>
 
 
 
-    /// Quaternion defining orientation of the line.
-    public var quaternion: Quaternion
-    /// The distance from the origin to the line.
-    public var d: Scalar
+    /// Direction vector of the line having unit length.
+    public let front: Vector
+    /// The coordinate on the line having minimum distance to the coordinate origin.
+    public let origin: Coordinate
 
 
 
     /// Memberwise initializer.
-    @inlinable
-    public init(quaternion: Quaternion, d: Scalar) {
-        assert(KvIs(quaternion.length, equalTo: 1), "The line quaternion have to be of unit length")
+    @usableFromInline
+    internal init(front: Vector, origin: Coordinate) {
+        self.front = front
+        self.origin = origin
+    }
 
-        self.quaternion = quaternion
-        self.d = d
+
+    /// A line having given direction and containing given coordinate.
+    @inlinable
+    public init(in direction: Vector, at coordinate: Coordinate) {
+        self.front = Math.normalize(direction)
+        self.origin = coordinate - Math.dot(coordinate, self.front)
+    }
+
+
+    /// A line having given direction and containing given vertex.
+    @inlinable
+    public init<V>(in direction: Vector, at vertex: V)
+    where V : KvVertex3Protocol, V.Math == Math {
+        self.init(in: direction, at: vertex.coordinate)
     }
 
 
@@ -90,82 +103,21 @@ public struct KvLine3<Math : KvMathScope> {
     }
 
 
-    /// A line having given direction and containing given coordinate.
-    @inlinable
-    public init(in direction: Vector, at coordinate: Coordinate) {
-        let direction = Math.normalize(direction)
-
-        var left = Math.cross(coordinate, -direction)
-
-        switch Math.isNonzero(left) {
-        case true:
-            let front = -direction
-            left = Math.normalize(left)
-            let up = Math.cross(front, left)
-
-            let q = Quaternion(Math.Matrix3x3(left, up, front))
-
-            self.init(quaternion: q, d: -Math.dot(coordinate, up))
-
-        case false:
-            self.init(quaternion: Quaternion(from: Self.front, to: direction), d: 0)
-        }
-    }
-
-
-    /// A line having given direction and containing given coordinate.
-    @inlinable
-    public init<V>(in direction: Vector, at vertex: V)
-    where V : KvVertex3Protocol, V.Math == Math {
-        self.init(in: direction, at: vertex.coordinate)
-    }
-
-
-
-    // MARK: Constatns
-
-    /// Unit direction vector in the local coordinate space. It is aimed in reverse Z direction.
-    ///
-    /// - SeeAlso: ``right``, ``up``.
-    public static var front: Vector { .unitNZ }
-    /// A unit vector in the local coordinate space that is orthogonal to ``front``. It matches Y axis.
-    ///
-    /// - SeeAlso: ``front``, ``right``.
-    public static var up: Vector { .unitY }
-    /// A unit vector in the local coordinate space that is orthogonal to both ``front`` and ``up``. It matches X axis.
-    ///
-    /// - SeeAlso: ``front``, ``up``.
-    public static var right: Vector { .unitX }
-
-
 
     // MARK: Operations
 
-    /// The direction vector.
-    ///
-    /// - SeeAlso: ``right``, ``up``.
-    @inlinable public var front: Vector { quaternion.act(Self.front) }
-    /// Vector orthogonal to ``front``.
-    ///
-    /// - SeeAlso: ``front``, ``right``.
-    @inlinable public var up: Vector { quaternion.act(Self.up) }
-    /// Vector orthogonal to both ``front`` and ``up``.
-    ///
-    /// - SeeAlso: ``front``, ``up``.
-    @inlinable public var right: Vector { quaternion.act(Self.right) }
-
-    /// A coordinate on the receiver having minimum distance to the coordinate origin.
-    @inlinable public var closestToOrigin: Coordinate { up * -d }
     /// - Returns: Some coordinate on the receiver.
-    @inlinable public var anyCoordinate: Coordinate { closestToOrigin }
+    @inlinable public var anyCoordinate: Coordinate { origin }
 
-    @inlinable public var isDegenerate: Bool { Math.isZero(quaternion) }
+
+    /// - Returns: *origin* + *offset* · *front*.
+    @inlinable public func at(_ offset: Scalar) -> Coordinate { origin + offset * front }
 
 
     /// - Returns: The distance from the receiver to given coordinate.
     @inlinable
     public func offset(to x: Coordinate) -> Scalar {
-        Math.length(Math.cross(x - closestToOrigin, front))
+        Math.length(Math.cross(x - origin, front))
     }
 
     /// - Returns: The distance from the receiver to given vertex.
@@ -179,9 +131,8 @@ public struct KvLine3<Math : KvMathScope> {
     /// - Returns: A boolean value indicating whether the receiver contains given coordinate.
     @inlinable
     public func contains(_ c: Coordinate) -> Bool {
-        let c = c - closestToOrigin
-        return Math.isZero(Math.cross(front, c),
-                           eps: Math.epsArg(front).cross(Math.epsArg(c)).tolerance)
+        let c = c - origin
+        return Math.isZero(Math.cross(front, c), eps: Math.epsArg(front).cross(Math.epsArg(c)).tolerance)
     }
 
     /// - Returns: A boolean value indicating whether the receiver contains given coordinate.
@@ -197,8 +148,8 @@ public struct KvLine3<Math : KvMathScope> {
     /// - See  ``projection(of:)``.
     @inlinable
     public func nearby(to rhs: Self) -> Coordinate? {
-        let (lFront, lOrigin) = (front, closestToOrigin)
-        let (rFront, rOrigin) = (rhs.front, rhs.closestToOrigin)
+        let (lFront, lOrigin) = (front, origin)
+        let (rFront, rOrigin) = (rhs.front, rhs.origin)
 
         let n = Math.cross(lFront, rFront)
         let n2 = Math.cross(rFront, n)
@@ -210,43 +161,18 @@ public struct KvLine3<Math : KvMathScope> {
         let lt = Math.dot(rFront, Math.cross(n, rOrigin - lOrigin)) / denominator
         // let rt = Math.dot(lFront, Math.cross(n, rOrigin - lOrigin)) / denominator
 
-        return lOrigin + lt * lFront
+        return at(lt)
     }
 
 
     /// - Returns: Projection of given coordinate on the receiver.
-    @inlinable
-    public func projection(of c: Coordinate) -> Coordinate {
-        let origin = closestToOrigin
-        let front = front
-
-        // Assuming front is a unit vector.
-        return origin + Math.dot((c - origin), front) * front
-    }
-
-
-    /// Translates all the receiver's ponts by *offset*.
-    ///
-    /// - Note: It's faster then apply an arbitrary transformation.
-    @inlinable public mutating func translate(by offset: Vector) { self = translated(by: offset) }
+    @inlinable public func projection(of c: Coordinate) -> Coordinate { at(Math.dot((c - origin), front)) }
 
 
     /// - Returns: A line produced applying translation by *offset* to all the receiver's ponts.
     ///
     /// - Note: It's faster then apply an arbitrary transformation.
-    @inlinable
-    public func translated(by offset: Vector) -> Self {
-        Self(in: front, at: anyCoordinate + offset)
-    }
-
-
-    /// Scales all the receiver's ponts.
-    ///
-    /// - Note: It's faster then apply an arbitrary transformation.
-    @inlinable
-    public mutating func scale(by scale: Scalar) {
-        d *= scale
-    }
+    @inlinable public func translated(by offset: Vector) -> Self { Self(front: front, origin: origin + offset) }
 
 
     /// - Returns: A line produced applying scale to all the receiver's ponts.
@@ -254,15 +180,8 @@ public struct KvLine3<Math : KvMathScope> {
     /// - Note: It's faster then apply an arbitrary transformation.
     @inlinable
     public func scaled(by scale: Scalar) -> Self {
-        Self(quaternion: quaternion, d: d * scale)
+        Self(front: front, origin: origin * scale)
     }
-
-
-    /// Applies given transformation to all the receiver's points.
-    @inlinable public mutating func apply(_ t: Transform) { self = t * self }
-
-    /// Applies given transformation to all the receiver's points.
-    @inlinable public mutating func apply(_ t: AffineTransform) { self = t * self }
 
 
 
@@ -271,13 +190,13 @@ public struct KvLine3<Math : KvMathScope> {
     /// - Returns: Result of given transformation applied to *rhs*.
     @inlinable
     public static func *(lhs: Transform, rhs: Self) -> Self {
-        Self(in: lhs.act(normal: rhs.front), at: lhs.act(coordinate: rhs.anyCoordinate))
+        Self(in: lhs.act(normal: rhs.front), at: lhs.act(coordinate: rhs.origin))
     }
 
     /// - Returns: Result of given transformation applied to *rhs*.
     @inlinable
     public static func *(lhs: AffineTransform, rhs: Self) -> Self {
-        Self(in: lhs.act(normal: rhs.front), at: lhs.act(coordinate: rhs.anyCoordinate))
+        Self(in: lhs.act(normal: rhs.front), at: lhs.act(coordinate: rhs.origin))
     }
 
 }
@@ -289,18 +208,7 @@ public struct KvLine3<Math : KvMathScope> {
 extension KvLine3 : KvNumericallyEquatable {
 
     /// - Returns: A boolean value indicating whether the receiver and *rhs* are numerically equal.
-    @inlinable
-    public func isEqual(to rhs: Self) -> Bool {
-        guard Math.isCollinear(front, rhs.front) else { return false }
-
-        switch KvIsNonzero(d) {
-        case true:
-            return Math.isEqual(closestToOrigin, rhs.closestToOrigin)
-
-        case false:
-            return KvIsZero(rhs.d)
-        }
-    }
+    @inlinable public func isEqual(to rhs: Self) -> Bool { Math.isEqual(origin, rhs.origin) && Math.isCollinear(front, rhs.front) }
 
 }
 
